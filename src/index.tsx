e33e845aa79d51587cc54259b285a7e555f77a1a
messages2raja <messages2raja@gmail.com>
2023-05-01 14:26:03 +0100

    changes

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <GlobalStyles />
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <App />
       </BrowserRouter>
     </Provider>
